e246ecc22975a8409c934bf4ca0a82c2c04b9563
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2019-06-15 10:58:27 -0300

    Refatorando a tela de listar avaliadores

--- src/main/java/br/ufjf/dcc193/trab02/Controllers/AvaliadorController.java
+++ src/main/java/br/ufjf/dcc193/trab02/Controllers/AvaliadorController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -76,6 +77,51 @@ public class AvaliadorController {
 
     @RequestMapping(value = {"/cadastro-avaliador"}, method = RequestMethod.POST)
     public ModelAndView realizaCadastro (Avaliador avaliador, HttpSession session)
+    {
+        ModelAndView mv = new ModelAndView();
+        Avaliador av = repositoryAvaliador.findByEmail(avaliador.getEmail());
+        if (av == null)
+        {
+            repositoryAvaliador.save(avaliador);
+            mv.setViewName("redirect:lista-avaliadores");
+        }
+        else
+        {
+            mv.addObject("avaliador", avaliador);
+            mv.setViewName("redirect:cadastro-avaliador");
+        }
+        return mv;
+    }
+
+    @RequestMapping(value = {"/cadastrar-area-conhecimento-avaliador/{id}"}, method = RequestMethod.GET)
+    public ModelAndView carregaAreaConhecimento (@PathVariable(value = "id", required = true) Long id, HttpSession session)
+    {
+        ModelAndView mv = new ModelAndView();
+        Avaliador avaliadorCarregar = new Avaliador();
+        if (session.getAttribute("usuarioLogado") != null)
+        {   
+            Avaliador avaliador = (Avaliador) session.getAttribute("usuarioLogado");
+            if (avaliador.getEmail().equals("admin"))
+            {
+                mv.addObject("avaliador", avaliadorCarregar);
+                mv.setViewName("cadastro-avaliador");
+            }
+            else
+            {
+                mv.addObject("avaliador", avaliador);
+                mv.setViewName("redirect:principal-avaliador");
+            }
+        }
+        else
+        {
+            mv.addObject("avaliador", avaliadorCarregar);
+            mv.setViewName("login");
+        }
+        return mv;
+    }
+
+    @RequestMapping(value = {"/cadastrar-area-conhecimento-avaliador"}, method = RequestMethod.POST)
+    public ModelAndView realizaCadastroAreaConhecimento (Avaliador avaliador, HttpSession session)
     {
         ModelAndView mv = new ModelAndView();
         Avaliador av = repositoryAvaliador.findByEmail(avaliador.getEmail());
